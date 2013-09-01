4931d193c3bb635f5329438f981bd5b0153178ed
kajjjak <mail@kjartanjonsson.com>
2013-09-01 09:56:31 -0700

    renaming file

---
+++ projectstore.js
@@ -0,0 +1,25 @@
+/* 
+Uses clientstore.js for local storage of projects and interfaces with Codewip web service api 
+*/
+var ProjectStore = new function() {
+    this.init = function(size_in_mb, callback_success, callback_failure, callback_warning){
+    };
+    
+    this.saveFile = function(obj){
+    	//creates a history and saves file
+    	
+    };
+    
+    this.loadFile = function(callback_success){
+    	
+    };
+    
+    this.saveProj = function(obj){
+    	//creates a history and saves file
+    };
+    
+    this.loadProj = function(callback_success){
+    	
+    };
+    
+};
