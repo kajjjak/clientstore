4f5e5e61443cf18243546c1b52ff9008f1ab0c3a
kajjjak <mail@kjartanjonsson.com>
2013-09-09 07:18:04 -0700

    Can fetch data

--- test_clientstore.js
+++ test_clientstore.js
@@ -1,21 +1,22 @@
 
-describe("Client store, ", function() {
+describe("ClientStoreInterfaceIndexedDB, ", function() {
 	beforeEach(function() {
-		ClientStore.init(50, "test_db", ["test_table"], function(){
+		window.clientStoreInterfaceIndexedDB = new ClientStoreInterfaceIndexedDB();
+		window.clientStoreInterfaceIndexedDB.init(50, "test_db", ["test_table"], function(){
 			window.clientstore_indexeddb_ready = true;
 		}, null, ClientStore.USE_INDEXEDDB);
   });
 
   afterEach(function() {
-  	//ClientStoreUtilsRemoveIndexedDB(CLIENT_STORE_DATABASE_NAME);
+  	ClientStoreUtilsRemoveIndexedDB(CLIENT_STORE_DATABASE_NAME);
   });	
   
   it("Set/Get item", function() {
   	waitsFor(function() { return window.clientstore_indexeddb_ready });
     runs(function(){
-    	ClientStore.setItem("test_table", "item_a", "A");
+    	window.clientStoreInterfaceIndexedDB.setItem("test_table", "item_a", "A");
     	window.result = undefined;
-    	ClientStore.getItem("test_table", "item_a", function(res){
+    	window.clientStoreInterfaceIndexedDB.getItem("test_table", "item_a", function(res){
 				window.result = res;    		
     	});
     });
