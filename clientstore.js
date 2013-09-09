931421344742726adce93aa32a63381bacf5ccce
kajjjak <mail@kjartanjonsson.com>
2013-09-09 04:02:14 -0700

    Using jsmin

--- clientstore.js
+++ clientstore.js
@@ -16,6 +16,8 @@ function ClientStoreInterfaceWebSQL (){
             if(callback_success){callback_success();}
           });
         });
+        //var self = this;
+        //setTimeout(function(){ if (!this.___cache_copied){ self.__restoreSession(); }}, 1000);
     },
     this.clear = function (callback_success){
         var sql = 'DELETE FROM buffer';
@@ -29,7 +31,7 @@ function ClientStoreInterfaceWebSQL (){
     this.setSize = function(){
     	/*not supported in websql*/
     },
-    this.getSize = function(callback_success, callback_failure){
+    this.getStorageSize = function(callback_success, callback_failure){
     	callback_failure(-1);
     },
     this.setItem = function (k, v){
@@ -63,15 +65,18 @@ function ClientStoreInterfaceIndexedDB (){
         window.___local_storage_db = undefined;
         var self = this;
         request = indexedDB.open(dbname);
-	      request.onsuccess = function(e){
-          window.___local_storage_db = e.target.result;
-          if(callback_success){callback_success();}
-        };
+        request.onsuccess = function(e){
+            window.___local_storage_db = e.target.result;
+            if(callback_success){callback_success();}
+/*            if (window.___local_storage_db.objectStoreNames.contains("buffer")) {
+                window.___local_storage_db.deleteObjectStore("buffer");
+            }
+*/        };
         request.onupgradeneeded = function(event) {
-          window.___local_storage_db = event.target.result;
-          var object_store = window.___local_storage_db.createObjectStore(CLIENT_STORE_BUFFER_NAME, {keyPath: "key"});
-          object_store.createIndex("value", "value", {unique: false});
-          if(callback_success){callback_success();}
+            window.___local_storage_db = event.target.result;
+            var object_store = window.___local_storage_db.createObjectStore(CLIENT_STORE_BUFFER_NAME, {keyPath: "key"});
+            object_store.createIndex("value", "value", {unique: false});
+            if(callback_success){callback_success();}
         };
         request.onerror = function(e){
             console.info("ClientStoreInterfaceIndexedDB error " + e.target.errorCode);
@@ -85,6 +90,9 @@ function ClientStoreInterfaceIndexedDB (){
           );
         };
     },
+    this.__restoreSession = function(callback_success){
+
+    },
     this.setSize = function(size_in_mb, callback_success, callback_failure){
         // Request Quota (only for File System API)  
         if(window.webkitStorageInfo){
@@ -95,7 +103,7 @@ function ClientStoreInterfaceIndexedDB (){
             });
         }
     },
-    this.getSize = function(callback_success, callback_failure){
+    this.getStorageSize = function(callback_success, callback_failure){
         // Request storage usage and capacity left
         if(window.webkitStorageInfo){
           window.webkitStorageInfo.queryUsageAndQuota(
@@ -129,18 +137,10 @@ function ClientStoreInterfaceIndexedDB (){
         object_store.add({"key":n, "value":v});
         sessionStorage.setItem(n, v);
     },
-    this.getItem = function (n, default_value, callback_success, callback_failure){
-			var object_store = this.getObjectStore(CLIENT_STORE_BUFFER_NAME, "readwrite");
-      object_store.get(n).onsuccess = function(event) {
-				var r = event.target.result;
-				if (r == null) {
-					callback_success(default_value);
-				  callback_failure("Could not find item with name " + n);
-				}
-				else {
-					callback_success(r.value);
-				}
-			};
+    this.getItem = function (n, default_value){
+        var v = sessionStorage.getItem(n);
+        if (v){ return v;}
+        return default_value;
     },
     this.removeItem = function(k){
         var request = this.getObjectStore(CLIENT_STORE_BUFFER_NAME, 'readwrite').delete(k);
@@ -149,6 +149,14 @@ function ClientStoreInterfaceIndexedDB (){
         };
     };
 }
+
+/*// Request storage usage and capacity left
+window.webkitStorageInfo.queryUsageAndQuota(webkitStorageInfo.TEMPORARY, //the type can be either TEMPORARY or PERSISTENT
+function(used, remaining) {
+  console.log("Used quota: " + used + ", remaining quota: " + remaining);
+}, function(e) {
+  console.log('Error', e); 
+} );*/
 
 var ClientStore = new function() {
 
@@ -170,28 +178,114 @@ var ClientStore = new function() {
     };
     
     this.setSize = function(size_in_megabites, callback_success, callback_failure){
-  		this.db.setSize(size_in_megabites, callback_success, callback_failure);
+    	if(this.db){
+    		this.db.setSize(size_in_megabites, callback_success, callback_failure);
+    	}
     };
     
     this.getSize = function(callback_success, callback_failure){
-  		this.db.getSize(callback_success, callback_failure);
+    	if(this.db){
+    		this.db.getStorageSize(callback_success, callback_failure);
+    	}
     };
 
-    this.setItem = function(k, v){
-     	return this.db.setItem(k, v);
+    this.__restoreSession = function(){
+        this.___cache_copied = true;
+        this.___local_storage_db.transaction(function(tx) {
+            tx.executeSql('SELECT * FROM buffer', [], function(tx, results) {
+              var len = results.rows.length, i;
+              for (i = 0; i < len; i++) {
+                var k = results.rows.item(i).id;
+                var v = results.rows.item(i).text;
+                console.info(v);
+                sessionStorage.setItem(k, v);
+              }
+            });
+        });
     };
 
-    this.getItem = function(k, default_value, callback_success, callback_failure){
-      if (default_value === undefined){default_value = null;}
-      this.db.getItem(k, default_value, callback_success, callback_failure);
+    this.__clearDB = function(){
+        this.db.clear();
     };
 
-    this.removeItem = function(k){
-      return this.db.removeItem(k);
+    this.__setItemDB = function(k, v){
+        this.db.setItem(k,v);
+    };
+
+    this.__setItem = function(k, v){
+    	this.__setItemDB(k, v); //for our offline backup
+    	/*
+        try{
+            window.localStorage.setItem(k, v);
+            //console.info("store: Using primary store for " + k);
+        } catch(e) {
+            try{
+                this.__setItemDB(k, v); //for our offline backup
+                try{sessionStorage.setItem(k, v);}
+                catch(err){}
+                //console.info("store: Using secondary store for " + k);
+            } catch(err){
+                console.info("store: Cache is full ");
+            }
+        }
+       */
+    };
+
+    this.__removeItem = function(k){
+        window.localStorage.removeItem(k);
+        this.db.removeItem(k);
+    };
+
+    this.__getItem = function(k, default_value, callback_success, callback_failure){
+        var v = window.localStorage.getItem(k);
+        if(v && (v.length > 1)) {if(callback_success){callback_success(v);} return v;}
+        return this.db.getItem(k, default_value, callback_success, callback_failure);
+    };
+
+    this.__clear = function(except_values){
+        tmp_cache = {};
+        var n;
+        for (var i in except_values){
+            n = except_values[i];
+            var v = this.__getItem(n);
+            if (v){
+                tmp_cache[n] = v;
+            }
+        }
+        window.localStorage.clear();
+        window.sessionStorage.clear();
+        this.__clearDB();
+        //restore values
+        for (i in except_values){
+            n = except_values[i];
+            this.__setItem(n, tmp_cache[n]);
+        }
+    };
+
+    this.getSizeUsed = function(){
+        return this.__getSizeUsed();
+    };
+
+    this.__getSizeUsed = function(){
+        //return JSON.stringify(localStorage).length;
+        return unescape(encodeURIComponent(JSON.stringify(localStorage))).length;
+    };
+
+    this.setItem = function(name, value){
+        return this.__setItem(name, value);
+    };
+
+    this.getItem = function(name, default_value, callback_success, callback_failure){
+        if (default_value === undefined){default_value = null;}
+        //return this.__getItem(name, default_value, callback_success, callback_failure);
+    };
+
+    this.removeItem = function(name){
+        return this.__removeItem(name);
     };
 
     this.clear = function(except_values){
-      this.db.clear();
+        this.__clear(except_values);
     };
 };
 
