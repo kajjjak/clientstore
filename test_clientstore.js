6d821b383f6ff3478401c6c9b115878f68e64805
kajjjak <mail@kjartanjonsson.com>
2013-09-09 07:05:39 -0700

    Save before fuck

--- test_clientstore.js
+++ test_clientstore.js
@@ -1,15 +1,27 @@
-CLIENT_STORE_DATABASE_NAME = "test_clientstore";
 
 describe("Client store, ", function() {
 	beforeEach(function() {
-		ClientStore.init(50, "test_db", null, null, ClientStore.USE_INDEXEDDB);
+		ClientStore.init(50, "test_db", ["test_table"], function(){
+			window.clientstore_indexeddb_ready = true;
+		}, null, ClientStore.USE_INDEXEDDB);
   });
 
   afterEach(function() {
   	//ClientStoreUtilsRemoveIndexedDB(CLIENT_STORE_DATABASE_NAME);
   });	
   
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(false);
+  it("Set/Get item", function() {
+  	waitsFor(function() { return window.clientstore_indexeddb_ready });
+    runs(function(){
+    	ClientStore.setItem("test_table", "item_a", "A");
+    	window.result = undefined;
+    	ClientStore.getItem("test_table", "item_a", function(res){
+				window.result = res;    		
+    	});
+    });
+    waitsFor(function() { return window.clientstore_indexeddb_ready && window.result });
+    runs(function(){
+    });
+    
   });
 });
