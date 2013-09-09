2238815336bcf06e0bfab131a20b6b8688fd508f
kajjjak <mail@kjartanjonsson.com>
2013-09-09 09:47:12 -0700

    Starting work on project store tests

--- test_projectstore.js
+++ test_projectstore.js
@@ -1,12 +1,41 @@
-describe("Project store, ", function() {
+function createProjectDB(){
+	ProjectStore.nuke();
+	window.projectstore_indexeddb_ready = false;	
+	setTimeout(function(){
+
+		ProjectStore.init(50, function(){
+			setTimeout(function(){
+				window.projectstore_indexeddb_ready = true;	
+			}, 500);
+			
+		}, function(){
+			alert("Could not create indexddb");
+		});
+
+	}, 500);	
+}
+
+describe("ProjectStore, ", function() {
 	beforeEach(function() {
-		
   });
 
   afterEach(function() {
   });	
   
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(false);
+  it("Set/Get item", function() {
+  	createProjectDB();
+  	waitsFor(function() { return window.projectstore_indexeddb_ready; });
+    runs(function(){
+    	ClientStore.setItem("cw_projects", "my_item_a", "Aa");
+    	window.project_result = undefined;
+    	ClientStore.getItem("cw_projects", "my_item_a", function(res){
+				window.project_result = res;    		
+    	});
+    });
+    waitsFor(function() { return window.projectstore_indexeddb_ready && window.project_result });
+    runs(function(){
+    	expect(window.project_result).toEqual("Aa");
+    });
   });
+
 });
