2bdb168702ce0299449541e96fdf05ae0903e7b8
kajjjak <mail@kjartanjonsson.com>
2013-09-10 03:53:04 -0700

    Generizing test data

--- test_projectstore.js
+++ test_projectstore.js
@@ -1,4 +1,10 @@
 window.projectstore_databasename = "codewip_testdb";
+
+window.projectstore_test_data1_uid = "myid";
+window.projectstore_test_data1 = {uid: window.projectstore_test_data1_uid, data: "mydata"};
+window.projectstore_test_data2_uid = "myfileid";
+window.projectstore_test_data2 = {uid:window.projectstore_test_data2_uid, data:"mydata"};
+
 function createProjectDB(){
 	ProjectStore.nuke();
 	window.projectstore_indexeddb_ready = false;	
@@ -29,15 +35,15 @@ describe("ProjectStore", function() {
   	waitsFor(function() { return window.projectstore_indexeddb_ready; });
     runs(function(){
     	console.info("Runs Save/Open local project");
-    	ProjectStore.saveProject({uid:"myid", data:"mydata"});
+    	ProjectStore.saveProject(window.projectstore_test_data1);
     	window.project_result = undefined;
-    	ProjectStore._loadProjectLocal("myid", function(res){
+    	ProjectStore._loadProjectLocal(window.projectstore_test_data1_uid, function(res){
 				window.project_result = res;    		
     	});
     });
     waitsFor(function() { return window.projectstore_indexeddb_ready && window.project_result });
     runs(function(){
-    	expect(window.project_result).toEqual({uid:"myid", data:"mydata"});
+    	expect(window.project_result).toEqual(window.projectstore_test_data1);
     });
   });
   
@@ -45,15 +51,15 @@ describe("ProjectStore", function() {
   	window.file_result = undefined;
   	waitsFor(function() { return window.projectstore_indexeddb_ready; });
     runs(function(){
-    	ProjectStore.saveFile({uid:"myfileid", data:"mydata"});
+    	ProjectStore.saveFile(window.projectstore_test_data2);
     	window.file_result = undefined;
-    	ProjectStore.openFile("myfileid", function(res){
+    	ProjectStore.openFile(window.projectstore_test_data2_uid, function(res){
 				window.file_result = res;    		
     	});
     });
     waitsFor(function() { return window.projectstore_indexeddb_ready && window.file_result });
     runs(function(){
-    	expect(window.file_result).toEqual({uid:"myfileid", data:"mydata"});
+    	expect(window.file_result).toEqual(window.projectstore_test_data2);
     });
   });
 
