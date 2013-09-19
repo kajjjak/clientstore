929f1b66c469e86684cdc65852143990d8403b46
kajjjak <mail@kjartanjonsson.com>
2013-09-19 02:01:27 -0700

    Creating tests for WebSQL

--- test_clientstore.js
+++ test_clientstore.js
@@ -1,5 +1,5 @@
 
-function createDB(){
+function createIndexedDB(){
 	ClientStoreUtilsRemoveIndexedDB("test_clientstore");
 	window.clientstore_indexeddb_ready = false;	
 	setTimeout(function(){
@@ -11,30 +11,32 @@ function createDB(){
 			
 		}, function(){
 			alert("Could not create indexddb");
-		}, ClientStore.USE_INDEXEDDB);
+		}, ClientStore.USE_DB_INDEXEDDB);
 
 	}, 500);	
 }
-createDB();
-/*
-function ClientStoreInterfaceIndexedDBase(){
-	ClientStoreUtilsRemoveIndexedDB("test_clientstore");
+
+function createWebSQLDB(){
+	ClientStoreUtilsRemoveWebSQLDB("test_clientstore", ["test_clientstore_table", "test_table"]);
+	window.clientstore_websqldb_ready = false;	
 	setTimeout(function(){
-		window.clientStoreInterfaceIndexedDB = new ClientStoreInterfaceIndexedDB();
-		window.clientStoreInterfaceIndexedDB.init(5, "test_clientstore", ["test_table"], function(){
+		ClientStore.init(5, "test_clientstore", ["test_clientstore_table", "test_table"], function(){
 			setTimeout(function(){
-				window.clientstore_indexeddb_ready = true;	
-			}, 1500);
+				window.clientstore_websqldb_ready = true;	
+				window.clientStoreInterfaceWebSQLDB = ClientStore.db;
+			}, 500);
 			
 		}, function(){
 			alert("Could not create indexddb");
-		});	
-		
-	}, 1500);
+		}, ClientStore.USE_DB_WEBSQLDB);
+
+	}, 1500);	
 }
-*/
+//createIndexedDB();
+createWebSQLDB();
+/*
 describe("ClientStoreInterfaceIndexedDB", function() {
-	//ClientStoreInterfaceIndexedDBase();
+	
 	beforeEach(function() {
   });
 
@@ -74,7 +76,7 @@ describe("ClientStoreInterfaceIndexedDB", function() {
   
 });
 
-describe("ClientStore", function() {
+describe("ClientStoreIndexedDB", function() {
 	beforeEach(function() {
   });
 
@@ -117,3 +119,109 @@ describe("ClientStore", function() {
     });
   });
 });
+*/
+describe("ClientStoreInterfaceWebSQLDB", function() {
+	
+	beforeEach(function() {
+  });
+
+  afterEach(function() {
+  });	
+  
+  it("Set/Get item", function() {
+  	waitsFor(function() { return window.clientstore_websqldb_ready && window.clientStoreInterfaceWebSQLDB; });
+    runs(function(){
+    	window.clientStoreInterfaceWebSQLDB.setItem("test_table", "item_a", "A");
+    	window.client_result_websqldb = undefined;
+    	window.clientStoreInterfaceWebSQLDB.getItem("test_table", "item_a", function(res){
+				window.client_result_websqldb = res;    		
+    	});
+    });
+    waitsFor(function() { return window.clientstore_websqldb_ready && window.client_result_websqldb });
+    runs(function(){
+    	expect(window.client_result_websqldb).toEqual("A");
+    });    
+  });
+  
+  it("Set and get all items", function() {
+  	waitsFor(function() { return window.clientstore_websqldb_ready; });
+    runs(function(){
+    	window.clientStoreInterfaceWebSQLDB.setItem("test_table", "item_a", "A");
+    	window.clientStoreInterfaceWebSQLDB.setItem("test_table", "item_b", "B");
+    	window.client_allitems_result = undefined;
+    	window.clientStoreInterfaceWebSQLDB.getAll("test_table", function(items){
+				window.client_allitems_result = items;    		
+    	});
+    });
+    waitsFor(function() { return window.clientstore_websqldb_ready && window.client_allitems_result });
+    runs(function(){
+    	expect(window.client_allitems_result).toEqual([ { key : 'item_a', value : 'A' }, { key : 'item_b', value : 'B' } ]);
+    });    
+  });
+  
+});
+/*
+describe("ClientStoreWebSQL", function() {
+	
+	beforeEach(function() {
+  });
+
+  afterEach(function() {
+  });
+  
+  it("Set/Get item", function() {
+  	waitsFor(function() { return window.clientstore_websqldb_ready; });
+    runs(function(){
+    	ClientStore.setItem("test_clientstore_table", "my_item_aa", "Aa");
+    	window.client_result_websqldb = undefined;
+    	ClientStore.getItem("test_clientstore_table", "my_item_aa", function(res){
+				window.client_result_websqldb = res;    		
+    	});
+    });
+    waitsFor(function() { return window.clientstore_websqldb_ready && window.client_result_websqldb });
+    runs(function(){
+    	expect(window.client_result_websqldb).toEqual("Aa");
+    });
+  });
+  
+  it("Clear item", function() {
+
+  	waitsFor(function() { return window.clientstore_websqldb_ready; });
+    runs(function(){
+    	window.client_result_websqldb = undefined;
+    	ClientStore.removeItem("test_clientstore_table", "my_item_a");
+    	setTimeout(function(){
+	    	ClientStore.getItem("test_clientstore_table", "my_item_a", 
+	    	function(res){
+					window.client_result_websqldb = "found it";
+	    	},function(){
+	    		window.client_result_websqldb = "did not found it";
+	    	});  		
+    	}, 1000);
+    });
+    waitsFor(function() { return window.clientstore_websqldb_ready && window.client_result_websqldb });
+    runs(function(){
+    	expect(window.client_result_websqldb).toEqual("did not found it");
+    });
+  });
+});
+*/
+
+
+/*
+function ClientStoreInterfaceIndexedDBase(){
+	ClientStoreUtilsRemoveIndexedDB("test_clientstore");
+	setTimeout(function(){
+		window.clientStoreInterfaceIndexedDB = new ClientStoreInterfaceIndexedDB();
+		window.clientStoreInterfaceIndexedDB.init(5, "test_clientstore", ["test_table"], function(){
+			setTimeout(function(){
+				window.clientstore_indexeddb_ready = true;	
+			}, 1500);
+			
+		}, function(){
+			alert("Could not create indexddb");
+		});	
+		
+	}, 1500);
+}
+*/
